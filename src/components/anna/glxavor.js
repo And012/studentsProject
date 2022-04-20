ccfb4fe74581fa27a2a6a1ef2be7f1d22b9f10f0
Anna <aristakesyan_2001@mail.ru>
2022-04-20 23:14:01 +0400

    some comment

---
+++ src/components/anna/glxavor.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './style.css'
+
+// comment
+function First(){
+   return( 
+    <div className="main">
+    <h1>Վիկտոր Համբարձումյան</h1>
+    <img src={'./nkar/nkar1.jpg'}/>
+    </div>
+   );
+
+}
+
+export default First
