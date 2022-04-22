1a9fc140e379879df2c90b4e4e1048e0f028b746
Anna <aristakesyan_2001@mail.ru>
2022-04-22 22:41:34 +0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import { Routes, Route,  useNavigate } from "react-router-dom";
+import Victor from "./components/anna/glxavor";
+import Charents from "./components/Christine/Charents";
+import Sahyan from './components/rima/Sahyan';
+import Sevak from './components/artur/index';
+import UserName from './components/styop/index'
+import { useState } from "react";
+
+
 function App() {
+const navigate = useNavigate()
   return (
     <div className="App">
-        this is our application
+      <div>
+        <ul style={{display: 'flex',
+                    lexDirection: 'row',
+                    justifyContent: 'space-between',
+                    gap: '20px'
+                    
+      }}>
+          <li onClick={navigate()}>Եղիշե Չարենց</li>
+          <li onClick={navigate()}>Վիկտոր Համբարձումյան</li>
+          <li onClick={navigate()}>Համո Սահյան</li>
+          <li onClick={navigate()}>Պարույր Սևակ</li>
+          <li onClick={navigate()}>Միսաք Մեծարենց</li>
+        </ul>
+      </div>
+      <Routes>
+        <Route path="/" element={<div>Glxavor ej</div>}/>
+        <Route path="charenc/*" element={<Charents/>}/>
+        <Route path="victor/*" element={<Victor/>}/>
+        <Route path="sahyan/*" element={<Sahyan/>}/>
+        <Route path="sevak/*" element={<Sevak/>}/>
+        <Route path="misak/*" element={<UserName/>}/>
+      </Routes>
     </div>
   );
 }
