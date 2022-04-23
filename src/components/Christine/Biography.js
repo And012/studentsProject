58bd5286544ad1edb855d9aa8ee7e9fbd71129f7
Andranik <andtsatur1296@gmail.com>
2022-04-23 22:27:47 +0400

    changes added sass

--- src/components/Christine/Biography.js
+++ src/components/Christine/Biography.js
@@ -1,19 +1,17 @@
-import { useNavigate } from "react-router-dom"
-import {info} from "./Charents"
-import "./Charents.css"
+import { useNavigate } from "react-router-dom";
+import { info } from "./Charents";
+import "./Charents.css";
 function Bio() {
-    const navigate = useNavigate()
-    return(
+  const navigate = useNavigate();
+  return (
     <>
-    <div className="container">
-        <h2 style={{margin:"10px 0"}}>Կենսագրություն</h2>
-        <a className="back" onClick={() => {
-            navigate("/charenc/")
-        }}>հետ վերադառնալ գլխավոր էջ</a>
+      <div className="container">
+        <h2 style={{ margin: "10px 0" }}>Կենսագրություն</h2>
+        <a href="/charenc">հետ վերադառնալ գլխավոր էջ</a>
         <hr />
         <p className="bio1">{info.bio}</p>
-    </div>
+      </div>
     </>
-    )
+  );
 }
-export default Bio
+export default Bio;
