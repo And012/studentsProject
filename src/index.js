cf0c72c8d35e36d6da925381945efa5cba72782b
Anna <aristakesyan_2001@mail.ru>
2022-04-23 16:39:36 +0400

    verjnakan path

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-//import App from "./App";
-import Victor from "./components/anna/glxavor";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
@@ -10,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Victor />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
