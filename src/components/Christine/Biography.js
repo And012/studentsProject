c7a378078455f9eafdb4d77c4b6c948962b55087
Christine <christine.fidanyan@gmail.com>
2022-04-24 14:44:34 +0300

    map and module.css in charents component

--- src/components/Christine/Biography.js
+++ src/components/Christine/Biography.js
@@ -1,15 +1,33 @@
 import { useNavigate } from "react-router-dom";
-import { info } from "./Charents";
-import "./Charents.css";
+import INFO from "./Constant/Constant";
+import styles from "./styles/Charents.module.css";
+
 function Bio() {
   const navigate = useNavigate();
   return (
     <>
-      <div className="container">
+      <nav className={styles.chNav}>
+        <ul className={styles.ch_ul}>
+          <li onClick={() => {
+            navigate("/charenc/")
+          }}>Գլխավոր</li>
+          <li onClick={() => {
+            navigate("/charenc/name")
+          }}>Գրական անուն</li>
+          <li onClick={() => {
+            navigate("/charenc/bio")
+          }}>Կենսագրություն</li>
+          <li onClick={() => {
+            navigate("/charenc/life")
+          }}>Անձնական կյանք</li>
+        </ul>
+      </nav>
+     
+      <div className={styles.container}>
         <h2 style={{ margin: "10px 0" }}>Կենսագրություն</h2>
-        <a href="/charenc">հետ վերադառնալ գլխավոր էջ</a>
+        <a href="/charenc" className={styles.back}>հետ վերադառնալ գլխավոր էջ</a>
         <hr />
-        <p className="bio1">{info.bio}</p>
+        <p className={styles.bio1}>{INFO.bio}</p>
       </div>
     </>
   );
--- src/components/Christine/Name.js
+++ src/components/Christine/Name.js
@@ -1,18 +1,35 @@
 import { useNavigate } from "react-router-dom"
-import {info} from "./Charents"
+import INFO from "./Constant/Constant"
+import styles from "./styles/Charents.module.css";
+
 
 function Name(){
     const navigate = useNavigate()
     return(
-        
-        <div className="container">
-            <h2 style={{margin:"10px 0"}} >Անձնական կյանք</h2>
-            <a className="back" onClick={() => {
-            navigate("/charenc/")
-            }}>հետ վերադառնալ գլխավոր էջ</a>
-            <hr></hr>
-            <p className="bio1">{info.life}</p>
-        </div>
+        <>
+            <nav className={styles.chNav}>
+                <ul className={styles.ch_ul}>
+                  <li onClick={() => {
+                    navigate("/charenc/")
+                  }}>Գլխավոր</li>
+                  <li onClick={() => {
+                    navigate("/charenc/name")
+                  }}>Գրական անուն</li>
+                  <li onClick={() => {
+                    navigate("/charenc/bio")
+                  }}>Կենսագրություն</li>
+                  <li onClick={() => {
+                    navigate("/charenc/life")
+                  }}>Անձնական կյանք</li></ul>
+            </nav>
+       
+            <div className={styles.container}>
+                <h2 style={{margin:"10px 0"}} >Անձնական կյանք</h2>
+                <a className={styles.back} href="/charenc">հետ վերադառնալ գլխավոր էջ</a>
+                <hr></hr>
+                <p className={styles.bio1}>{INFO.life}</p>
+            </div>
+        </>
     )
 }
 export default Name
