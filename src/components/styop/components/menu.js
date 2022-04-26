a1a98146a69deebb962f3f6c45e62cefd8fa955a
Styopik <styop196@gmail.com>
2022-04-26 13:47:50 +0400

    xd

--- src/components/styop/components/menu.js
+++ src/components/styop/components/menu.js
@@ -1,26 +1,53 @@
 import { useState } from "react"
-import { img } from "../text"
+import {useNavigate } from 'react-router-dom'
+import { arrimg,norut} from "../text"
+
+
 
 export default function Menu(){
     return <div>
     </div>
 }
 
-export function Qaxaqakanutyun(){
-     const [index,setindex] = useState(0)  
-     if(index > 20){
-        index = 0
-     }if(index < 0 ){
-        index = 20
-     }
-     console.log(index)
+export function Qaxaqakanutyun(){  
+    const navigate = useNavigate()
+    const [index,setindex] = useState(0)
+    const norutyunhref = [["https://www.youtube.com/watch?v=93wAfYyD7NI&t=11s"],[]]
     return<div>
-        <section className="Qaxaqakanutyun" >
+        <section className="Qaxaqakanutyun" > 
+        <h2>Քաղաքականություն</h2>
         <div className="slayder" >
-            <div className="slayder-content" style={{backgroundImage:`url(../img/imgslayder/${index}.jpg)`}}>
-                <a href="#" className="angle-left" onClick={() =>setindex((prev) =>index - 1)}><i class="fa-solid fa-angle-left"></i></a>
-                <a href="#" className="angle-right" onClick={() =>setindex((prev) => index + 1)}><i class="fa-solid fa-angle-right"></i></a>
+           <div className="slayder-content-left">
+              {norut.map(([images,vern,texts],index) =>{
+                  return  <a href='' className="libs">
+                      <div key={index} className='slayder-content-left-content'>
+                      <div style={{backgroundImage:`url('${images}')`}} className="imgnorut"></div>
+                      <div className="lists-content-slayd" >
+                          <h5>{vern}</h5>
+                          <p>{texts}</p>
+                      </div>
+                  </div>
+                  </a>
+              })}
+             
+           </div>
+            <div className="slayder-content" style={{backgroundImage:`url("${arrimg[index]}")`}}>
+                <a href="#" className="angle-left" onClick={() =>{
+                    if(index <= 0){
+                        setindex(arrimg.length - 1)
+                    }else{
+                         setindex(index - 1)
+                    }
+                }}><i class="fa-solid fa-angle-left"></i></a>
+                <a href="#" className="angle-right" onClick={() =>{
+                    if(index >= arrimg.length - 1){
+                        setindex(0)
+                      }else{
+                        setindex(index + 1)
+                     }
+                    }}><i class="fa-solid fa-angle-right"></i></a>
             </div>
+            <div className="slayder-content-right"></div>
         </div>
         </section>
     </div>
--- src/components/styop/index.js
+++ src/components/styop/index.js
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import { Route, Routes, useNavigate } from 'react-router-dom'
-import {Glxavor,Qaxaqakanutyun,Tntesutyun,Hasarakutyun,Mshakuyt} from './components/menu'
+import {Qaxaqakanutyun,Tntesutyun,Hasarakutyun,Mshakuyt} from './components/menu'
 import './index.css'
-import { Kensahrutyun,Irmasin,Sashxatanq,Banarvest,Grakanutyun,obj } from './text'
+import { Obj,arrmenu1,arrmenu3,arrmenu5,arrmenu4,arrmenu6,arrmenu2} from './text'
 
 export default function Mecarenc(){
     return <div>
@@ -22,26 +22,21 @@ export default function Mecarenc(){
 function Nav(){
     const navigate = useNavigate()
     return <div>
-        <nav className="nav">
-            <div id='logo'>
-                <a href='#'></a>
-            </div>
-            <div id='menu'>
-               <ul id='menu-content'>
-                   <li><a href='#'onClick={() => navigate("Glxavor")}>Գլխավոր</a></li>
-                   <li><a href='#'onClick={() => navigate("Qaxaqakanutyun")}>Քաղաքականություն</a></li>
-                   <li><a href='#'onClick={() => navigate("Tntesutyun")}>Տնտեսություն</a></li>
-                   <li><a href='#'onClick={() => navigate("Hasarakutyun")}>Հասարակություն</a></li>
-                   <li><a href='#'onClick={() => navigate("Mshakuyt")}>Մշակույթ</a></li>
-                   <li id='icon'>
+               <nav className="nav">
+                   <div id='logo'>
                        <a href='#'></a>
-                       <a href='#'></a>
-                       <a href='#'></a>
-                    </li>
-               </ul>
-            </div>
-        </nav>
-    </div>
+                   </div>
+                   <div id='menu'>
+                        <ul id='menu-content' >
+                   {arrmenu1.map(([names,setname],index) =>{
+                              return  <li key={index}>
+                                 <a href='#' onClick={() => navigate(names)}>{setname} </a>
+                                 </li>   
+                              
+                           
+                          })}</ul></div>
+               </nav>
+             </div>
 }
 
 function Main(){
@@ -52,22 +47,24 @@ function Main(){
               <div className='main-content-left'>
                   <div id='img'></div>
               </div>
-              <div className='main-content-right'>
-                  <ul className='list'>
-                      <li><a href='#' onClick={() => navigate('Kensahrutyun')}>Կենսագրություն</a></li>
-                      <li><a href='#' onClick={() => navigate('Irmasin')}>Միսաք Մեծարենցի մասին</a></li>
-                      <li><a href='#' onClick={() => navigate('Sashxatanq')}>Ստեղծագործական աշխատանք</a></li>
-                      <li><a href='#' onClick={() => navigate('Banarvest')}>Մեծարենցի բանարվեստը</a></li>
-                      <li className='list-v'><a href='#' onClick={() =>navigate('Grakanutyun')}>Գրականություն</a></li>
-                  </ul>
+              <div className='main-content-right'> 
+
+                   <ul className='list'>
+                         {arrmenu2.map(([names,setname],index) =>{
+                              return <li key={index}>
+                                  <a href='#' onClick={() => navigate(names)}>{setname}</a>
+                                  </li>
+                                  })}  
+                                 <li className='list-v'><a href='#' onClick={() =>navigate('Grakanutyun')}>Գրականություն</a></li>
+                     </ul>
                   <h2>Միսաք Մեծարենց</h2>
                       <Routes>
-                          <Route path='/' element={<div className='content-cont'>{obj.Kensahrutyun}</div>}/>
-                          <Route path='Kensahrutyun' element={ <div className='content-cont'>{obj.Kensahrutyun}</div>  }/>
-                          <Route path='Irmasin' element={<div className='content-cont'>{obj.Irmasin}</div> }/>
-                          <Route path='Sashxatanq' element={<div className='content-cont'>{obj.Sashxatanq}</div> }/>
-                          <Route path='Banarvest' element={<div className='content-cont'>{obj.Banarvest}</div> }/>
-                          <Route path='Grakanutyun' element={<div className='content-cont'>{obj.Grakanutyun}</div> }/>
+                          <Route path='/' element={<div className='content-cont'>{Obj.Kensahrutyun}</div>}/>
+                          <Route path='Kensahrutyun' element={ <div className='content-cont'>{Obj.Kensahrutyun}</div>  }/>
+                          <Route path='Irmasin' element={<div className='content-cont'>{Obj.Irmasin}</div> }/>
+                          <Route path='Sashxatanq' element={<div className='content-cont'>{Obj.Sashxatanq}</div> }/>
+                          <Route path='Banarvest' element={<div className='content-cont'>{Obj.Banarvest}</div> }/>
+                          <Route path='Grakanutyun' element={<div className='content-cont'>{Obj.Grakanutyun}</div> }/>
                       </Routes>
                 
                 </div>
@@ -78,54 +75,56 @@ function Main(){
 
 
 function Footer(){
-    return <div className='footer'>
+     return <div className='footer'>
         <div className='footer-content'>
             <div className='footer-left'>
                 <h3>Հայ <span>Մեծեր</span></h3>
-                <ul>
-                    <li><a href='https://www.facebook.com/maytni.groxner' target='_blank'><i class="fa-brands fa-facebook-f"></i></a></li>
-                    <li><a href='https://www.instagram.com/hay_mec_groxner_/' target='_blank'><i class="fa-brands fa-instagram"></i></a></li>
-                    <li><a href='https://twitter.com/Sevak1924' target='_blank'><i class="fa-brands fa-twitter"></i></a></li>
-                    <li><a href='https://www.youtube.com/watch?v=9zhnS6U_s2E' target='_blank'><i class="fa-brands fa-youtube"></i></a></li>
-                    <li><a href='https://t.me/s/hmtorum' target='_blank'><i class="fa-brands fa-telegram"></i></a></li>  
-                </ul>
-                <div className='footer-icon'>
-                    <a href='#'>
-                        <div><i class="fa-brands fa-apple"></i></div>
-                        <div>
-                            <h6>Download on the</h6>
-                            <h5>App Store</h5>
-                        </div>
-                    </a>
-                    <a href='#'>
-                        <div><i class="fa-brands fa-google-play"></i></div>
-                        <div>
-                            <h6>GET IT ON</h6>
-                            <h5>Google Play</h5>
-                        </div>
-                    </a>
-                </div>
+                    <div className='footer-left-icon'>
+                    {arrmenu6.map(([hrefs,icon],index)=>{
+                        return <ul key={index}><li><a href={hrefs} ><i class={icon}></i></a></li></ul>
+                    })}
+                    </div>
+                   <div className='footer-icon'>
+                       <a href='#'>
+                           <div><i class="fa-brands fa-apple"></i></div>
+                           <div>
+                               <h6>Download on the</h6>
+                               <h5>App Store</h5>
+                           </div>
+                       </a>
+                       <a href='#'>
+                           <div><i class="fa-brands fa-google-play"></i></div>
+                           <div>
+                               <h6>GET IT ON</h6>
+                               <h5>Google Play</h5>
+                           </div>
+                       </a>
+                   </div>
             </div>
-            <div className='footer-right'>
-                <ul>
-                    <li><a href='#'>Մատուցվող ծառայություններ</a></li>
-                    <li><a href='#'>Կարգավորում</a></li>
-                    <li><a href='#'>Գաղտնիության քաղաքականություն</a></li>
-                    <li><a href='#'>Հաճախորդների իրավունքները</a></li>
-                </ul>
-                <ul>
-                    <li><a href='#'>Էքսկլյուզիվ</a></li>
-                    <li><a href='#'>Քաղաքականություն</a></li>
-                    <li><a href='#'>Մամուլի Տեսություն</a></li>
-                    <li><a href='#'>Մշակութային հավելված</a></li>
-                </ul>
-                <ul>
-                    <li><a href='#'>Մամուլը մեր մասին</a></li>
-                    <li><a href='#'>Կայքի քարտեզ</a></li>
-                    <li><a href='#'>Հաճախ տրվող հարցեր</a></li>
-                    <li><a href='#'>Հետադարձ կապ</a></li>
-                </ul>
-            </div>
+         
+               <div className='footer-right'>
+                       <ul>
+                         {arrmenu3.map(([names],index) =>{
+                          return <li key={index}>
+                                <a href='#'>{names}</a>
+                                </li>
+                          })}
+                        </ul>
+                        <ul>
+                           {arrmenu4.map(([names],index) =>{
+                          return <li key={index}>
+                                <a href='#'>{names}</a>
+                                </li>
+                          })}
+                         </ul>
+                         <ul>
+                           {arrmenu5.map(([names],index) =>{
+                          return <li key={index}>
+                                <a href='#'>{names}</a>
+                                </li>
+                          })}
+                         </ul>
+                 </div> 
         </div>
     </div>
 }
