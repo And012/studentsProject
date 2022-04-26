a23d41fc3b3bc9a78f051a531001f2882cdbe208
Anna <aristakesyan_2001@mail.ru>
2022-04-26 22:51:14 +0400

    changes

--- src/components/anna/byurakan.js
+++ src/components/anna/byurakan.js
@@ -2,20 +2,15 @@ import { Route, Routes, useNavigate } from "react-router-dom";
 import { Text_BYURAKAN } from "./const.js";
 import styles from "./Victor.module.css"
 
-function Astxaditaran(){
-    return(
-        <>
-    <Routes>
-        <Route path="/astx" element={<div>hello</div>}/>
-    </Routes>
-    </>
-    );
-}
+
 
 function Byurakan(){
-    const navigate = useNavigate
+    const navigate = useNavigate()
     return(
         <>
+            <Routes>
+        <Route path="/astx" element={<div>hello</div>}/>
+    </Routes>
         <div className={styles.list}>
             {Text_BYURAKAN}
             <a className={styles.forMore} onClick={()=>{navigate('/astx')}}>Իմանալ ավելին</a>
