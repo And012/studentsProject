c3c2f8802ba6621497cf1b2d853f6dcc603a9d56
Anna <aristakesyan_2001@mail.ru>
2022-05-10 20:23:36 +0400

    redux chages

--- src/components/anna/astxaspyur.js
+++ src/components/anna/astxaspyur.js
@@ -1,14 +1,105 @@
-import { useSelector } from "react-redux";
-import styles from "./Victor.module.css"
+import { useSelector, useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+import styles from "./Victor.module.css";
+import {
+  setInfo,
+  setInfoO_Type,
+  setInfoR_Type,
+  setInfoT_Type,
+} from "./astxaState";
 
+export default function AstxaSpyur() {
+  const [fullInfo, setFullInfo] = useState(true);
+  const [oType, setOType] = useState(true);
+  const [rType, setRType] = useState(true);
+  const [tType, setTType] = useState(true);
+  const info = useSelector((state) => {
+    return state.astxaState.information;
+  });
+  const image = useSelector((state) => {
+    return state.astxaState.image;
+  });
+  const dispatch = useDispatch();
+  // useEffect(()=>{setFullInfo(false)}, [fullInfo])
+  return (
+    <>
+      <div className={styles.list}>{info}</div>
+      <div className={styles.star_type}>
+        {fullInfo && (
+          <Button
+            size="large"
+            variant="text"
+            onClick={() => {
+              return (
+                dispatch(setInfo()),
+                setFullInfo(false),
+                setOType(true),
+                setRType(true),
+                setTType(true)
+              );
+            }}
+          >
+            Աստղասփյուռներ
+          </Button>
+        )}
+        {oType && (
+          <Button
+            size="large"
+            variant="text"
+            onClick={() => {
+              return (
+                dispatch(setInfoO_Type()),
+                setFullInfo(true),
+                setOType(false),
+                setRType(true),
+                setTType(true),
+                console.log(fullInfo)
+              );
+            }}
+          >
+            O տիպի
+          </Button>
+        )}
+        {tType && (
+          <Button
+            size="large"
+            variant="text"
+            onClick={() => {
+              return (
+                dispatch(setInfoT_Type()),
+                setFullInfo(true),
+                setOType(true),
+                setRType(true),
+                setTType(false)
+              );
+            }}
+          >
+            T տիպի
+          </Button>
+        )}
 
-export default function AstxaSpyur(){
-    const info = useSelector((state)=>{return state.astxaState.information});
-    const image = useSelector((state)=>{return state.astxaState.image})
-    return(
-        <>
-        <div className={styles.list}>{info}</div>
-        <div className={styles.astxImage}><img src={image}/></div>
-        </>
-    );
+        {rType && (
+          <Button
+            size="large"
+            variant="text"
+            onClick={() => {
+              return (
+                dispatch(setInfoR_Type()),
+                setFullInfo(true),
+                setOType(true),
+                setRType(false),
+                setTType(true)
+              );
+            }}
+          >
+            R տիպի{" "}
+          </Button>
+        )}
+      </div>
+      <div className={styles.astxImage}>
+        <img src={image} />
+      </div>
+    </>
+  );
 }
